abf8d1be0887af59ebca7f39566db61eb2cf67bc
Oliver Davies <oliver@oliverdavies.uk>
2019-07-18 13:16:05 +0100

    Ensure all config gets merged

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 const defaultConfig = require('tailwindcss/defaultConfig')
+const merge = require('lodash/merge')
 
-module.exports = function(plugin, pluginOptions, config = defaultConfig) {
-  return {
-    ...config,
+module.exports = function(plugin, pluginOptions, userConfig = {}) {
+  const testConfig = {
     corePlugins: false,
     plugins: [plugin(pluginOptions)],
   }
+
+  return merge({}, defaultConfig, testConfig, userConfig)
 }
